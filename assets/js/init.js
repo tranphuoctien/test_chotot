382e38c1ed6607f3e5b0096eae3f6b83e4f1abed
teenmaz <teenmaz@gmail.com>
2016-02-20 17:10:45 +0700

    -- remove unnecessarily code

--- assets/js/init.js
+++ assets/js/init.js
@@ -9,28 +9,16 @@ angular.module('chotot').run(['$rootScope','$location',function($rootScope,$loca
 }]);
 angular.module('chotot').controller('MainController',['$rootScope','$scope','$http','$window',
     function($rootScope,$scope,$http,$window){
-        var ajaxToServer = function(){return $http.get($rootScope.BaseUrlPublic+':3000/getResult');};
         var socket = io.connect(':3000',{reconnection:true});
         // Get result
         $scope.getResult = function(){
-            ajaxToServer().success(function(data){
+            $http.get($rootScope.BaseUrlPublic+':3000/getResult').success(function(data){
                 $scope.listing_thumbnails = data;
             });
         };
         socket.on('getReresh',function(data){
             $scope.getResult();
         });
-        $scope.eventRefresh = function(){
-            ajaxToServer().success(function(data){
-                data.reverse();
-                data.forEach(function(val,index){
-                    console.log($scope.listing_thumbnails.indexOf(val));
-                    if($scope.listing_thumbnails.indexOf(val)===-1)
-                        $scope.listing_thumbnails.unshift(val);
-                });
-            });
-        };
-
     }
 ]);
 angular.module('chotot').directive('cont', function () {
